c7b3d3d351db49981feaaa277d7f7ec4bdbab501
Yanisdz93 <55578164+Yanisdz93@users.noreply.github.com>
2020-03-13 14:26:09 +0100

    Changement script test api genius

---
+++ TestAPIGenius/scripts2.js
@@ -0,0 +1,61 @@
+var request1 = new XMLHttpRequest()
+var id = 0
+var img = ''
+var bio =''
+
+function biotxt (children){
+    for (let index = 0; index < children.length; index++) {
+        if(children[index].children != undefined){
+            biotxt(children[index].children)
+        }
+        else if(children[index] === ""){
+            bio += "<br>"
+        }
+        else if(typeof(children[index]) === 'string'){
+            bio += children[index]
+        }
+        
+    }
+}
+
+
+request1.open('GET','https://api.genius.com/search?q=soolking&access_token=nOKS_slApPiXY3hr3wLrHBl_GPk9dl9KWuGmgM_HrZVwI8JQhKtatdSycRXq0wLN',false)
+
+//request.setRequestHeader('Authorization','Bearer ')
+
+request1.onload = function() {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+  
+    if (request1.status >= 200 && request1.status < 400) {
+        id = data['response']['hits'][0]['result']['primary_artist']['id']
+        console.log(id)
+    }
+    else{
+        console.log("error search")
+    }
+}
+
+request1.send()
+
+var request2 = new XMLHttpRequest
+request2.open('GET','https://api.genius.com/artists/'+id+'?access_token=nOKS_slApPiXY3hr3wLrHBl_GPk9dl9KWuGmgM_HrZVwI8JQhKtatdSycRXq0wLN',false)
+request2.onload = function(){
+    var data = JSON.parse(this.response)
+    if(request2.status >= 200 && request2.status < 400) {
+        img = data['response']['artist']['image_url']
+        console.log(img)
+        biotxt(data['response']['artist']['description']['dom']['children'])
+            // var biotxt 
+        // bio.forEach(element => {
+        //     biotxt += element
+        // });
+
+        console.log(bio)
+    }
+    else{
+        console.log("error artists")
+    }
+}
+
+request2.send()
