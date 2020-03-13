a4c805002fae7645b4c7a63a34b20667a04c7f05
Yanisdz93 <55578164+Yanisdz93@users.noreply.github.com>
2020-03-13 15:59:14 +0100

    Update scripts2.js

--- TestAPIGenius/scripts2.js
+++ TestAPIGenius/scripts2.js
@@ -1,7 +1,6 @@
 var request1 = new XMLHttpRequest()
 var id = 0
-var img = ''
-var bio =''
+var bio = ""
 
 function biotxt (children){
     for (let index = 0; index < children.length; index++) {
@@ -19,7 +18,7 @@ function biotxt (children){
 }
 
 
-request1.open('GET','https://api.genius.com/search?q=soolking&access_token=nOKS_slApPiXY3hr3wLrHBl_GPk9dl9KWuGmgM_HrZVwI8JQhKtatdSycRXq0wLN',false)
+request1.open('GET','https://api.genius.com/search?q=ninho&access_token=nOKS_slApPiXY3hr3wLrHBl_GPk9dl9KWuGmgM_HrZVwI8JQhKtatdSycRXq0wLN',false)
 
 //request.setRequestHeader('Authorization','Bearer ')
 
@@ -43,15 +42,21 @@ request2.open('GET','https://api.genius.com/artists/'+id+'?access_token=nOKS_slA
 request2.onload = function(){
     var data = JSON.parse(this.response)
     if(request2.status >= 200 && request2.status < 400) {
-        img = data['response']['artist']['image_url']
+        var img = data['response']['artist']['image_url']
         console.log(img)
         biotxt(data['response']['artist']['description']['dom']['children'])
             // var biotxt 
         // bio.forEach(element => {
         //     biotxt += element
         // });
+        var facebook = data['response']['artist']['facebook_name']
+        var instagram= data['response']['artist']['instagram_name']
+        var twiter = data['response']['artist']['twitter_name']
 
         console.log(bio)
+        console.log("facebook " + facebook)
+        console.log("instagram " + instagram)
+        console.log("twitter " + twiter)
     }
     else{
         console.log("error artists")
