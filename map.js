87d13fc0fe52da83b7c8ffb61439dc7d26edff02
Resfa <faresamiar1@gmail.com>
2020-03-13 16:26:15 +0100

    test

--- map.js
+++ map.js
@@ -1,7 +1,9 @@
+var map ;
+var greenIcon;
 window.onload = function () {
-    var map = L.map('map').setView([48.7945, 2.3340], 11);
+    map = L.map('map').setView([48.7945, 2.3340], 11);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: 'PING'}).addTo(map);
-    var greenIcon = L.icon({
+    greenIcon = L.icon({
         iconUrl: 'leaf-green.png',
         shadowUrl: 'leaf-shadow.png',
 
@@ -12,8 +14,6 @@ window.onload = function () {
         popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
 
-    L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
-
 }
 
 const fetchAsync = async () =>
@@ -23,14 +23,15 @@ var json = fetchAsync();
 
 console.log(json)
 
-json.then((value) => { enfinTMRCNWAAAR(value)})
+json.then((value) => { locatione(value)})
 
-function NWAARNWAAR(json) {
+function locatione(json) {
     for(var i = 0; i < json.length; ++i) {
         var coord = json[i]['location']['coordinates'];
         var coords = coord.split(',')
         coord = [coords[1].substring(1)]
         coord.push(coords[0])
-        console.log(coord)
+        L.marker(coord,{icon : greenIcon}).addTo(map);
     }
+    console.log("afficher")
 }
