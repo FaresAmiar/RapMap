e208a71682bd38cd195a09f707c91dbb8326a326
Yanisdz93 <55578164+Yanisdz93@users.noreply.github.com>
2020-03-24 15:47:45 +0100

    API Genius

--- map.js
+++ map.js
@@ -1,5 +1,15 @@
 let map;
 let json;
+let id = 0;
+let img;
+
+var LeafIcon = L.Icon.extend({
+    options: {
+        iconSize:     [38, 95],
+        iconAnchor:   [22, 94],
+        popupAnchor:  [-3, -76]
+    }
+});
 
 $(document).ready(function () {
     definirMap()
@@ -29,19 +39,85 @@ function definirMap() {
 	
     fetchAsync();
 	
-    json.then((value) => { locatione(value)});
+    // json.then((value) => { locatione(value)});
 
     function locatione(json) {
-        for(var i = 0; i < json.length; ++i) {
+        for(var i = 0; i < 3; ++i) {
             var coord = json[i]['location']['coordinates'];
             var coords = coord.split(',')
             coord = [coords[1].substring(1)]
             coord.push(coords[0])
-            L.marker(coord,{icon : greenIcon}).addTo(map);
+            getImage(json[i]['name'])
+            console.log(`First name: ${img}, last name: ${id} ` + i)
+            L.marker(coord,new LeafIcon({iconUrl: "https://fr.m.wikipedia.org/wiki/Fichier:Approve_icon.svg"})).addTo(map)
         }
         console.log("afficher")
     }
 
+    async function getImage(nom){
+        let URLId = 'https://api.genius.com/search?q='+nom+'&access_token=GeU8QghXpNWNaWVNgdew5wSrh8uAKlsGqfYNp0VFXNHcRVzAEzOVT8xuLCCdwI1R';
+        let responseID = await fetch(URLId,{
+            method: 'GET'
+        })        
+        let dataID = await responseID.json()
+        id = dataID['response']['hits'][0]['result']['primary_artist']['id']
+        console.log(id)
+        
+        let URLArtiste = 'https://api.genius.com/artists/'+id+'?access_token=GeU8QghXpNWNaWVNgdew5wSrh8uAKlsGqfYNp0VFXNHcRVzAEzOVT8xuLCCdwI1R';
+        
+        let responseArtiste = await fetch(URLArtiste,{
+            method: 'GET'
+        })        
+        let dataArtiste = await responseArtiste.json()
+        img = dataArtiste['response']['artist']['image_url']
+        console.log(img)
 
+    }
+
+
+
+
+    // async function getIdArtiste(nom){
+    //     var URLId = 'https://api.genius.com/search?q='+nom+'&access_token=GeU8QghXpNWNaWVNgdew5wSrh8uAKlsGqfYNp0VFXNHcRVzAEzOVT8xuLCCdwI1R';
+
+    //     let response = await fetch(URLId,{
+    //         method: 'GET'
+    //     })
+    //     let data = await response.json()
+    //     id = data['response']['hits'][0]['result']['primary_artist']['id']
+    // }
+
+    // function getImage(nom){
+    //     let URLId = 'https://api.genius.com/search?q='+nom+'&access_token=GeU8QghXpNWNaWVNgdew5wSrh8uAKlsGqfYNp0VFXNHcRVzAEzOVT8xuLCCdwI1R';
+    //     fetch(URLId).then(function (response) {
+    //         if (response.ok) {
+    //             return response.json();
+    //         } else {
+    //             return Promise.reject(response);
+    //         }
+    //     }).then(function (data) {
+
+    //         // Store the post data to a variable
+    //         id = data['response']['hits'][0]['result']['primary_artist']['id'];
+    //         console.log(id)
+    //         let URLArtiste = 'https://api.genius.com/artists/'+id+'?access_token=GeU8QghXpNWNaWVNgdew5wSrh8uAKlsGqfYNp0VFXNHcRVzAEzOVT8xuLCCdwI1R';
+
+    //         // Fetch another API
+    //         return fetch(URLArtiste);
+
+    //     }).then(function (response) {
+    //         if (response.ok) {
+    //             return response.json();
+    //         } else {
+    //             return Promise.reject(response);
+    //         }
+    //     }).then(function (artistdata) {
+    //         img = artistdata['response']['artist']['image_url']
+    //     }).catch(function (error) {
+    //         console.warn(error);
+    //     });
+    //     return img
+    // }
+    
 
 
